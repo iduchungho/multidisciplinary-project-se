cb7c6919394a99880175a6454058715d6dc3342b
Kawasaki <iduchungho@gmail.com>
2023-04-28 23:39:48 +0700

    add design pattern

--- server/pkg/routes/userRoute.go
+++ server/pkg/routes/userRoute.go
@@ -2,18 +2,18 @@ package route
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"smhome/app/controllers"
+	controller "smhome/app/controllers"
 	"smhome/pkg/middleware"
 )
 
 func UserRoute(r *fiber.App) {
-	r.Get("/api/user/getAll/:id", middleware.RequireUserID, controller.GetAllUser)
-	r.Get("/api/user/logout/:id", middleware.RequireUserID, controller.Logout)
-	r.Get("/api/user/getUserById/:id", middleware.RequireUserID, controller.GetUserByID)
+	//r.Get("/api/user/getAll/:id", middleware.RequireUserID, controller.GetAllUser)
+	r.Get("/api/user/logout", middleware.RequireUser, controller.Logout)
+	r.Get("/api/user/getUserById", middleware.RequireUserByID, controller.GetUserByID)
 	r.Post("/api/user/login", controller.Login)
 	r.Post("/api/user/new", controller.AddNewUser)
-	r.Put("/api/user/changeAvatar/:id", middleware.RequireUserID, controller.ChangeAvatar)
-	r.Put("/api/user/update/:id", middleware.RequireUserID, controller.UpdateInformation)
-	r.Delete("/api/user/delete/:id", middleware.RequireUserID, controller.DeleteUser)
+	r.Put("/api/user/changeAvatar", middleware.RequireUserByID, controller.ChangeAvatar)
+	r.Put("/api/user/update", middleware.RequireUserByID, controller.UpdateInformation)
+	r.Delete("/api/user/delete", middleware.RequireUserByID, controller.DeleteUser)
 
 }
