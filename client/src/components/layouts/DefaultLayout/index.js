df19cd957019e1ffee21204eb75bc36a662a12b6
Chinh Lâm Điền <lamdienchinhtd9a2@gmail.com>
2023-05-03 17:38:33 +0700

    fix layout

--- client/src/components/layouts/DefaultLayout/index.js
+++ client/src/components/layouts/DefaultLayout/index.js
@@ -11,7 +11,6 @@ import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { useSelector } from "react-redux"
 function DefaultLayout() {
-
   const user = useSelector((state) => state.auth_.login?.currentUser)
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -54,106 +53,117 @@ function DefaultLayout() {
       break;
   }
 
-    var month="",num1=date.getMonth();
-    switch(num1)
-      {
-        case 0:
-          month="January";
-          break;
-        case 1:
-          month="February";
-          break;
-        case 2:
-          month="March";
-          break;
-        case 3:
-          month="April";
-          break;
-        case 4:
-          month="May";
-          break;
-        case 5:
-          month="June";
-          break;
-        case 6:
-          month="July";
-          break;
-        case 7:
-          month="August";
-          break;
-        case 8:
-          month="September";
-          break;
-        case 9:
-          month="October";
-          break;
-        case 10:
-          month="November";
-          break;
-        case 11:
-          month="December";
-          break;
-      }
-    return(
-      <div className="App">
-        <div className="wrapper">
-            <div className="cover">
-                <div className="cover__item item1"></div>
-                <div className="cover__item item2"></div>
-                <div className="cover__item item3"></div>
-                <div className="cover__item item4"></div>
+  var month = "", num1 = date.getMonth();
+  switch (num1) {
+    case 0:
+      month = "January";
+      break;
+    case 1:
+      month = "February";
+      break;
+    case 2:
+      month = "March";
+      break;
+    case 3:
+      month = "April";
+      break;
+    case 4:
+      month = "May";
+      break;
+    case 5:
+      month = "June";
+      break;
+    case 6:
+      month = "July";
+      break;
+    case 7:
+      month = "August";
+      break;
+    case 8:
+      month = "September";
+      break;
+    case 9:
+      month = "October";
+      break;
+    case 10:
+      month = "November";
+      break;
+    case 11:
+      month = "December";
+      break;
+  }
+  function handlelogout() {
+    if (window.confirm('Bạn có muốn đăng xuất không ?')) {
+      logout(dispatch, navigate)
+    }
+  }
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <div className="cover">
+          <div className="cover__item item1"></div>
+          <div className="cover__item item2"></div>
+          <div className="cover__item item3"></div>
+          <div className="cover__item item4"></div>
+        </div>
+        <div class="center">
+          <div className="main">
+            <div className="siderbar">
+              <ul className="siderbar__list">
+                <li className="siderbar__list-item" style={btnSider == 0 ? active : {}}>
+                  <Link to="/dashboard" onClick={() => setBtnSider(0)}>
+                    <i class="home-icon fa-solid fa-house"></i>
+                  </Link>
+                </li>
+                <li className="siderbar__list-item" style={btnSider == 1 ? active : {}}>
+                  <Link to='/door' onClick={() => setBtnSider(1)}>
+                    <i class="door-icon fa-solid fa-door-open"></i>
+                  </Link>
+                </li>
+                <li className="siderbar__list-item" style={btnSider == 2 ? active : {}}>
+                  <Link to="/light" onClick={() => setBtnSider(2)}>
+                    <i class="light-icon fa-regular fa-lightbulb"></i>
+                  </Link>
+                </li>
+                <li className="siderbar__list-item" style={btnSider == 3 ? active : {}}>
+                  <Link to="/temperHumi" onClick={() => setBtnSider(3)}>
+                    <i class="temper-icon fa-solid fa-temperature-three-quarters"></i>
+                  </Link>
+                </li>
+                <li className="siderbar__list-item" style={btnSider == 4 ? active : {}}>
+                  <Link to="/notification" onClick={() => setBtnSider(4)}>
+                    <i class="gear-icon fa-solid fa-bell"></i>
+                  </Link>
+                </li>
+                <li className="siderbar__list-item" style={btnSider == 5 ? active : {}}>
+                  <div className="logout-wrapper" onClick={handlelogout}>
+                    <i class="logout-icon fa-solid fa-power-off"></i>
+                  </div>
+                </li>
+              </ul>
             </div>
-            <div class="center">
-                <div className="main">
-                    <div className="siderbar">
-                        <ul className="siderbar__list">
-                            <li className="siderbar__list-item"  style = { btnSider==0?active:{}}>
-                                <Link to ="/dashboard" onClick={()=>setBtnSider(0)}>
-                                  <i class="home-icon fa-solid fa-house"></i>
-                                </Link>
-                            </li>
-                            <li className="siderbar__list-item"  style = { btnSider==1?active:{}}>
-                                <Link to='/door' onClick={()=>setBtnSider(1)}>
-                                  <i class="door-icon fa-solid fa-door-open"></i>
-                                </Link>
-                            </li>
-                            <li className="siderbar__list-item"  style = { btnSider==2?active:{}}>
-                                <Link to ="/light" onClick={()=>setBtnSider(2)}>
-                                  <i class="light-icon fa-regular fa-lightbulb"></i>
-                                </Link>
-                            </li>
-                            <li className="siderbar__list-item"  style = { btnSider==3?active:{}}>
-                                <Link to="/temperHumi" onClick={()=>setBtnSider(3)}>
-                                  <i class="temper-icon fa-solid fa-temperature-three-quarters"></i> 
-                                </Link>
-                            </li>
-                            <li className="siderbar__list-item"  style = { btnSider==4?active:{}}>
-                              <Link to="/notification" onClick={()=>setBtnSider(4)}>
-                                  <i class="gear-icon fa-solid fa-gear"></i>
-                               </Link>   
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="container">
-                        <header className="header">
-                            <nav className="navbar">
-                                <div class="navbar__logo">
-                                    <i class="logo-icon fa-solid fa-layer-group"></i>
-                                    SMART HOME
-                                </div>
-                                <div className="navbar__account">
-                                    <img className="navbar__account-img" src="https://img.thuthuattinhoc.vn/uploads/2019/04/10/anh-bong-hoa_112954549.jpg"/>
-                                </div>
-                            </nav>
-                            <div className="header__time">
-                                <div className="header__time-greeting">
-                                    <span className="greeting1">Good morning, Bao</span>
-                                    <span className="greeting2">Have a nice day</span>
-                                </div>
-                                <div className="header__time-weather">
-                                    <img src={weather} className="weather-icon"/>
-                                </div>
-                                <div className="header__time-infor">
+            <div className="container">
+              <header className="header">
+                <nav className="navbar">
+                  <div class="navbar__logo">
+                    <i class="logo-icon fa-solid fa-layer-group"></i>
+                    SMART HOME
+                  </div>
+                  <div className="navbar__account">
+                    <Link to="/user">
+                      <img className="navbar__account-img" src={user.data.avatar} />
+                    </Link>
+                  </div>
+                </nav>
+                <div className="header__time">
+                  <div className="header__time-greeting">
+                    <span className="greeting1">Good morning, {user.data.firstname}</span>
+                    <span className="greeting2">Have a nice day</span>
+                  </div>
+                  <div className="header__time-weather">
+                    <img src={weather} className="weather-icon" />
+                  </div>
+                  <div className="header__time-infor">
 
                     <span className="time1">{hour}:{date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()}
                       {
